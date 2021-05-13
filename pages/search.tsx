3a6d0d2e8863370610a4bef09d465553eda093b6
Mateo Galic <mateogalic112@gmail.com>
2021-05-13 12:33:28 +0200

    Adding search functionality for posts.

--- pages/search.tsx
+++ pages/search.tsx
@@ -1,7 +1,62 @@
+import Link from 'next/link';
+
+import { GetServerSideProps } from 'next';
+import { Post } from '../models/Post';
+
+import qs from 'qs';
+
 import React from 'react';
 
-const Search = () => {
-	return <div></div>;
+import { API_URL } from '../config/index';
+
+import Layout from '../src/components/Layout';
+import FancyCard from '../src/components/FancyCard';
+import VerticalSpacer from '../src/widgets/VerticalSpacer';
+import { Container, Grid, Typography } from '@material-ui/core';
+
+interface SearchResults {
+	posts: Post[];
+}
+
+const Search: React.FC<SearchResults> = ({ posts }) => {
+	return (
+		<Layout>
+			<Container>
+				<VerticalSpacer />
+				<Typography variant="h3">Search results</Typography>
+				<VerticalSpacer />
+				<Grid container spacing={5}>
+					{posts.map((post) => (
+						<Grid item key={post.id}>
+							<Link href={`/posts/${post.slug}`}>
+								<a>
+									<FancyCard post={post} />
+								</a>
+							</Link>
+						</Grid>
+					))}
+				</Grid>
+			</Container>
+			<VerticalSpacer />
+		</Layout>
+	);
+};
+
+export const getServerSideProps: GetServerSideProps = async ({ query }) => {
+	const { term } = query;
+
+	const keyword = qs.stringify({
+		_where: {
+			_or: [{ title_contains: term }, { author_contains: term }],
+		},
+	});
+
+	const res = await fetch(`${API_URL}/posts?${keyword}`);
+	const posts: Post[] = await res.json();
+
+	return {
+		props: { posts },
+	};
 };
 
 export default Search;
---
+++ src/components/FancyCard.tsx
@@ -0,0 +1,84 @@
+import React from 'react';
+
+import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
+import { red } from '@material-ui/core/colors';
+
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import ShareIcon from '@material-ui/icons/Share';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+
+import {
+	Typography,
+	Card,
+	CardHeader,
+	CardMedia,
+	CardContent,
+	CardActions,
+	Avatar,
+	IconButton,
+} from '@material-ui/core';
+
+import { Post } from '../../models/Post';
+
+const useStyles = makeStyles((theme: Theme) =>
+	createStyles({
+		root: {
+			maxWidth: 345,
+		},
+		media: {
+			height: 0,
+			paddingTop: '56.25%', // 16:9
+		},
+		avatar: {
+			backgroundColor: red[500],
+		},
+	})
+);
+
+type FancyCardProps = {
+	post: Post;
+};
+
+const FancyCard: React.FC<FancyCardProps> = ({ post }) => {
+	const classes = useStyles();
+
+	return (
+		<Card className={classes.root}>
+			<CardHeader
+				avatar={
+					<Avatar aria-label="recipe" className={classes.avatar}>
+						{post.author.username.charAt(0).toUpperCase()}
+					</Avatar>
+				}
+				action={
+					<IconButton aria-label="settings">
+						<MoreVertIcon />
+					</IconButton>
+				}
+				title={post.title}
+				subheader={new Date(post.created_at).toLocaleString('en-US', {
+					weekday: 'short',
+					day: 'numeric',
+					year: 'numeric',
+					month: 'long',
+				})}
+			/>
+			<CardMedia className={classes.media} image={post.featured_image.formats.small.url} title={post.slug} />
+			<CardContent>
+				<Typography variant="body2" color="textSecondary" component="p">
+					{post.excerpt}
+				</Typography>
+			</CardContent>
+			<CardActions disableSpacing>
+				<IconButton aria-label="add to favorites">
+					<FavoriteIcon />
+				</IconButton>
+				<IconButton aria-label="share">
+					<ShareIcon />
+				</IconButton>
+			</CardActions>
+		</Card>
+	);
+};
+
+export default FancyCard;
