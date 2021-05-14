aaa8981fed43018c96811b0795c4da43f4352fbf
Mateo Galic <mateogalic112@gmail.com>
2021-05-14 11:23:53 +0200

    Styles for 404 page.

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,7 +1,42 @@
 import React from 'react';
+import Link from 'next/link';
 
-const NotFoundPage = () => {
-	return <div>Not found.</div>;
+import theme from '../utils/theme';
+import { makeStyles } from '@material-ui/core/styles';
+import { Container, Typography, Button } from '@material-ui/core';
+
+import Layout from '../src/components/Layout';
+import VerticalSpacer from '../src/widgets/VerticalSpacer';
+
+const useStyles = makeStyles({
+	container: {
+		minHeight: '80vh',
+		display: 'flex',
+		flexDirection: 'column',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+});
+
+const NotFoundPage: React.FC = () => {
+	const classes = useStyles();
+
+	return (
+		<Layout>
+			<Container className={classes.container}>
+				<Typography variant="h1">404</Typography>
+				<Typography gutterBottom variant="h3">
+					Page not found
+				</Typography>
+				<Link href="/" passHref>
+					<Button color="primary">
+						<a>Back to home</a>
+					</Button>
+				</Link>
+			</Container>
+			<VerticalSpacer />
+		</Layout>
+	);
 };
 
 export default NotFoundPage;
